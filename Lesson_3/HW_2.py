7c5f91b12195a7f5851392b5f8f25fb1817bef76
Irene_Mast <irishechka314@ya.ru>
2021-08-25 00:36:40 +0300

    Init

---
+++ Lesson_3/HW_2.py
@@ -0,0 +1,14 @@
+#Lesson 3.
+#Задание 2.Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы.
+#Реализовать вывод данных о пользователе одной строкой
+
+	name = input('enter name')
+	surname = input('enter surname')
+    year = int(input('enter year'))
+    city = input('enter city')
+    email = input('enter email')
+    telephone = input('input telephone')
+def my_func (name, surname, year, city, email, telephone):
+	return ' '.join([name, surname, year, city, email, telephone])
+
+print(my_func(surname = 'Adler', name = 'Irene', year = '1990', city = 'Spb', email = '123456@mail.ru', telephone = '123456789'))
